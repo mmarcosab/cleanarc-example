dcfa02d73f42c64cc9ab562597f7f0fb2b4f3106
Marcos Vinicius <marcosab.vinicius@gmail.com>
2021-05-14 11:58:53 -0300

    First commit

---
+++ src/main/java/br/com/application/cleanarcexample/CleanarcExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.application.cleanarcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarcExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/application/cleanarcexample/CleanarcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.application.cleanarcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
